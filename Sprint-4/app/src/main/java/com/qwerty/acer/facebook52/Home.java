40e69056dd487654f585cb31d54004c6d3816f24
abdulrauf1996 <abdulrauft1996@gmail.com>
2016-10-16 22:11:42 -0700

    sprint 3

---
+++ Sprint-4/app/src/main/java/com/qwerty/acer/facebook52/Home.java
@@ -0,0 +1,22 @@
+package com.qwerty.acer.facebook52;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+              getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.activity_action);
+
+
+    }
+}
